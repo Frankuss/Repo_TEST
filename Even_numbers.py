2f71356a869c012f6a2a1e02aee6a41919dcd688
Francesco <francesco.chiaveri@gmail.com>
2017-06-18 11:19:48 +0200

    Inserting a Python program that prints even numbers from 0 to 100

---
+++ Even_numbers.py
@@ -0,0 +1,12 @@
+#A stupid program to test GITHUB functionalities: printing even numbers from 0 to 100
+
+list=[0]
+i=1
+
+while i<=100:
+    if i%2==0:
+        list.append(i)
+    i=i+1
+
+print(list)
+
